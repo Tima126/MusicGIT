f82e2a3af66b1a51323a6b949283df62cb58bd64
Alex <djstar2015@gmail.com>
2024-12-24 14:17:03 +0300

    ВЫВОД ТАБЛИЦЫ!!!
    ОНО ЖИВОЕ

--- BlazorClient/obj/Debug/net6.0/BlazorClient.AssemblyInfo.cs
+++ BlazorClient/obj/Debug/net6.0/BlazorClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b582389e07641db781abf42db6d9e78c59df4bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24cfd1f7068975a011b059f31e1c57f2da2bed84")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
